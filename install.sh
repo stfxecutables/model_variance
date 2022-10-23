897cb25e761c297ff55b0ee9a65661d93dc07882
Derek M Berger <dmberger.dev@gmail.com>
2022-10-23 16:10:14 -0400

    setup dependencies with poetry

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+THIS_SCRIPT_PARENT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+cd "$THIS_SCRIPT_PARENT" || exit 1
+
+poetry self update
+poetry install --with dev
