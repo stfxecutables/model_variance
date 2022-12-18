4b196fab5a9977c3c672e7416483997ae8b2b913
Derek M Berger <dmberger.dev@gmail.com>
2022-12-18 13:49:50 -0500

    implement hparam interfaces

--- src/hparams/hparams.py
+++ src/hparams/hparams.py
@@ -12,6 +12,7 @@ from argparse import ArgumentParser, Namespace
 from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
+from pprint import pformat, pprint
 from typing import (
     Any,
     Callable,
@@ -36,7 +37,9 @@ import pytest
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 from numpy import ndarray
+from numpy.random import Generator
 from pandas import DataFrame, Series
+from scipy.stats import loguniform
 from typing_extensions import Literal
 
 T = TypeVar("T")
@@ -49,10 +52,10 @@ class HparamKind(Enum):
 
 
 class Hparam(ABC, Generic[T]):
-    def __init__(self, name: str, value: T) -> None:
+    def __init__(self, name: str, value: T | None) -> None:
         super().__init__()
         self.name: str = name
-        self._value: T = value
+        self._value: T | None = value
         self.kind: HparamKind
 
     @property
@@ -64,7 +67,11 @@ class Hparam(ABC, Generic[T]):
         raise ValueError("Cannot set value on Hparam")
 
     @abstractmethod
-    def __sub__(self, o: Hparam) -> int:
+    def random(self, rng: Generator | None = None) -> Hparam:
+        ...
+
+    @abstractmethod
+    def __sub__(self, o: Hparam) -> float:
         if not isinstance(o, Hparam):
             raise ValueError(
                 f"Can only find difference between hparams of same kind. Got {type(o)}"
@@ -74,25 +81,57 @@ class Hparam(ABC, Generic[T]):
                 "Cannot find difference between different hparams. "
                 f"Got: `{self.name}` - `{o.name}`"
             )
+        if self.value is None:
+            raise ValueError("Cannot subtract hparam with no value")
         return abs(self.value - o.value)
 
 
 class ContinuousHparam(Hparam):
     def __init__(
-        self, name: str, value: float, max: float, min: float, log_scale: bool = False
+        self,
+        name: str,
+        value: float | None,
+        max: float,
+        min: float,
+        log_scale: bool = False,
     ) -> None:
         super().__init__(name=name, value=value)
         self.name: str = name
         self.kind: HparamKind = HparamKind.Continuous
-        self._value: float = float(value)
+        self._value: float | None = float(value) if value is not None else None
         self.min: float = float(min)
         self.max: float = float(max)
         self.log_scale: bool = log_scale
 
-    def normed(self) -> float:
-        return (self.value - self.min) / (self.max - self.min)
+    def random(self, rng: Generator | None = None) -> ContinuousHparam:
+        if rng is None:
+            if self.log_scale:
+                value = float(loguniform.rvs(self.min, self.max, size=1))
+            else:
+                value = float(np.random.uniform(self.min, self.max))
+        else:
+            if self.log_scale:
+                loguniform.random_state = rng
+                value = float(loguniform.rvs(self.min, self.max, size=1))
+            else:
+                value = float(rng.uniform(self.min, self.max, size=1))
+        cls: Type[ContinuousHparam] = self.__class__
+        return cls(
+            name=self.name,
+            value=value,
+            max=self.max,
+            min=self.min,
+            log_scale=self.log_scale,
+        )
 
-    def __sub__(self, o: ContinuousHparam) -> float:
+    def normed(self) -> float:
+        if self.log_scale:
+            value = float(np.log10(self.value))
+            mn, mx = float(np.log10(self.min)), float(np.log10(self.max))
+            return (value - mn) / (mx - mn)
+        return float((self.value - self.min) / (self.max - self.min))
+
+    def __sub__(self, o: Hparam) -> float:
         if not isinstance(o, ContinuousHparam):
             raise ValueError(
                 f"Can only find difference between hparams of same kind. Got {type(o)}"
@@ -102,25 +141,44 @@ class ContinuousHparam(Hparam):
                 "Cannot find difference between different hparams. "
                 f"Got: `{self.name}` - `{o.name}`"
             )
+        if self.value is None:
+            raise ValueError("Cannot subtract hparam with no value")
         return abs(o.normed() - self.normed())
+
+    def __str__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(name={self.name}, value={self.value}, "
+            f"min={self.min}, max={self.max}, log={self.log_scale})"
+        )
+
+    __repr__ = __str__
 
 
 class OrdinalHparam(Hparam):
-    def __init__(self, name: str, value: int, max: int, min: int = 0) -> None:
+    def __init__(self, name: str, value: int | None, max: int, min: int = 0) -> None:
         super().__init__(name=name, value=value)
         self.name: str = name
         self.kind: HparamKind = HparamKind.Ordinal
-        self._value: int = int(value)
+        self._value: int | None = int(value) if value is not None else None
         self.min: int = int(min)
         self.max: int = int(max)
+        self.values = list(range(self.min, self.max + 1))
+
+    def random(self, rng: Generator | None = None) -> OrdinalHparam:
+        if rng is None:
+            value = float(np.random.choice(self.values))
+        else:
+            value = float(rng.choice(self.values, size=1, shuffle=False))
+        cls: Type[OrdinalHparam] = self.__class__
+        return cls(name=self.name, value=value, max=self.max, min=self.min)
 
     def normed(self) -> float:
-        return (self.value - self.min) / (self.max - self.min)
+        return float((self.value - self.min) / (self.max - self.min))
 
     def __len__(self) -> int:
         return self.max - self.min + 1
 
-    def __sub__(self, o: OrdinalHparam) -> int:
+    def __sub__(self, o: Hparam) -> float:
         if not isinstance(o, OrdinalHparam):
             raise ValueError(
                 f"Can only find difference between hparams of same kind. Got {type(o)}"
@@ -130,21 +188,42 @@ class OrdinalHparam(Hparam):
                 "Cannot find difference between different hparams. "
                 f"Got: `{self.name}` - `{o.name}`"
             )
+        if self.value is None:
+            raise ValueError("Cannot subtract hparam with no value")
         return abs(o.normed() - self.normed())
+
+    def __str__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(name={self.name}, value={self.value}, "
+            f"min={self.min}, max={self.max})"
+        )
+
+    __repr__ = __str__
 
 
 class CategoricalHparam(Hparam):
-    def __init__(self, name: str, value: str | None, n_categories: int) -> None:
+    def __init__(
+        self, name: str, value: str | None, categories: Sequence[str] | Collection[str]
+    ) -> None:
         super().__init__(name=name, value=value)
         self.name: str = name
         self.kind: HparamKind = HparamKind.Categorical
-        self._value: str | None = value
-        self.n_categories: int = int(n_categories)
+        self._value: str | None = str(value) if value is not None else None
+        self.categories: list[str] = list(categories)
+        self.n_categories: int = len(categories)
+
+    def random(self, rng: Generator | None = None) -> CategoricalHparam:
+        if rng is None:
+            value = float(np.random.choice(self.categories, size=1))
+        else:
+            value = float(rng.choice(self.categories, size=1, shuffle=False))
+        cls: Type[CategoricalHparam] = self.__class__
+        return cls(name=self.name, value=value, categories=self.categories)
 
     def __len__(self) -> int:
         return self.n_categories
 
-    def __sub__(self, o: CategoricalHparam) -> int:
+    def __sub__(self, o: Hparam) -> float:
         if not isinstance(o, CategoricalHparam):
             raise ValueError(
                 f"Can only find difference between hparams of same kind. Got {type(o)}"
@@ -154,13 +233,25 @@ class CategoricalHparam(Hparam):
                 "Cannot find difference between different hparams. "
                 f"Got: `{self.name}` - `{o.name}`"
             )
-        return int(self.value != o.value)
+        if self.value is None:
+            raise ValueError("Cannot subtract hparam with no value")
+        return float(int(self.value != o.value))
+
+    def __str__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(name={self.name}, value={self.value}, "
+            f"categories={self.categories})"
+        )
+
+    __repr__ = __str__
 
 
 class Hparams(ABC):
-    def __init__(self, hparams: Collection[Hparam] | Sequence[Hparam]) -> None:
+    def __init__(
+        self, hparams: Collection[Hparam] | Sequence[Hparam] | None = None
+    ) -> None:
         super().__init__()
-        hps = sorted(hparams, key=lambda hp: hp.name)
+        hps = sorted(hparams, key=lambda hp: hp.name) if hparams is not None else []
         self.hparams: dict[str, Hparam] = {}
         for hp in hps:
             if hp.name in self.hparams:
@@ -184,6 +275,13 @@ class Hparams(ABC):
         self.n_ordinal = len(self.ordinals)
         self.n_categorical = len(self.categoricals)
 
+    def random(self, rng: Generator | None = None) -> Hparams:
+        cls = self.__class__
+        hps = []
+        for name, hp in self.hparams.items():
+            hps.append(hp.random(rng))
+        return cls(hparams=hps)
+
     def __sub__(self, o: Hparams) -> float:
         if not isinstance(o, Hparams):
             raise ValueError(f"Can only find difference between instance of Hparams.")
@@ -196,5 +294,12 @@ class Hparams(ABC):
         for name in sorted(self.hparams.keys()):
             hp1 = self.hparams[name]
             hp2 = o.hparams[name]
-            diffs.append(float(hp1 - hp2))
+            diffs.append(hp1 - hp2)
         return float(np.mean(diffs))
+
+    def __str__(self) -> str:
+        fmt = [f"{self.__class__.__name__}("]
+        for name, hp in self.hparams.items():
+            fmt.append(f"  {str(hp)}")
+        fmt.append(")")
+        return "\n".join(fmt)
--- src/hparams/xgboost.py
+++ src/hparams/xgboost.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from typing import (
     Any,
     Callable,
+    Collection,
     Dict,
     List,
     Optional,
@@ -36,12 +37,65 @@ from numpy import ndarray
 from pandas import DataFrame, Series
 from typing_extensions import Literal
 
-from src.hparams.hparams import Hparam, HparamKind, Hparams
+from src.hparams.hparams import (
+    ContinuousHparam,
+    Hparam,
+    HparamKind,
+    Hparams,
+    OrdinalHparam,
+)
 
 
-class XGBoostEta(Hparam):
-    pass
+def xgboost_hparams(
+    eta: float | None = None,
+    lamda: float | None = None,
+    alpha: float | None = None,
+    num_round: int | None = None,
+    gamma: float | None = None,
+    colsample_bylevel: float | None = None,
+    colsample_bynode: float | None = None,
+    colsample_bytree: float | None = None,
+    max_depth: int | None = None,
+    max_delta_step: int | None = None,
+    min_child_weight: float | None = None,
+    subsample: float | None = None,
+) -> list[Hparams]:
+    return [
+        ContinuousHparam("eta", eta, max=1.0, min=0.001, log_scale=True),
+        ContinuousHparam("lambda", lamda, max=1.0, min=1e-10, log_scale=True),
+        ContinuousHparam("alpha", alpha, max=1.0, min=1e-10, log_scale=True),
+        OrdinalHparam("num_round", num_round, max=1000, min=1),
+        ContinuousHparam("gamma", gamma, max=1.0, min=0.1, log_scale=True),
+        ContinuousHparam(
+            "colsample_bylevel", colsample_bylevel, max=1.0, min=0.1, log_scale=False
+        ),
+        ContinuousHparam(
+            "colsample_bynode", colsample_bynode, max=1.0, min=0.1, log_scale=False
+        ),
+        ContinuousHparam(
+            "colsample_bytree", colsample_bytree, max=1.0, min=0.1, log_scale=False
+        ),
+        OrdinalHparam("max_depth", max_depth, max=20, min=1),
+        OrdinalHparam("max_delta_step", max_delta_step, max=10, min=0),
+        ContinuousHparam(
+            "min_child_weight", min_child_weight, max=20, min=0.1, log_scale=True
+        ),
+        ContinuousHparam("subsample", subsample, max=1.0, min=0.01, log_scale=False),
+    ]
 
 
 class XGBoostHparams(Hparams):
-    pass
+    def __init__(
+        self,
+        hparams: Collection[Hparam] | Sequence[Hparam] | None = None,
+    ) -> None:
+
+        if hparams is None:
+            hparams = xgboost_hparams()
+        super().__init__(hparams)
+
+
+if __name__ == "__main__":
+    xgbs = XGBoostHparams()
+    print(xgbs)
+    print(xgbs)
