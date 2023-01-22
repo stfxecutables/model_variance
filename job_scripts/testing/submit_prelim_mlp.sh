b7013fa4049f61c88a61a4e11b05929d4b95f4d7
Derek M Berger <dmberger.dev@gmail.com>
2023-01-22 17:08:35 -0500

    [SHARE]

--- job_scripts/testing/submit_prelim_mlp.sh
+++ job_scripts/testing/submit_prelim_mlp.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-type=TIME_LIMIT_90
 #SBATCH --profile=all
 #SBATCH --job-name=mlp_hps
-#SBATCH --output=mlp_hps_%A_%a_%j.out
+#SBATCH --output=/scratch/dberger/model_variance/slurm_logs/mlp_hps_%A_%a_%j.out
 #SBATCH --array=0-9
 #SBATCH --time=00-08:00:00
 #SBATCH --nodes=1
