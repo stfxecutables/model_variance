bf937f2dfba27a7c22d66525d0365952439e8080
Derek Berger <dmberger.dev@gmail.com>
2023-03-29 13:15:24 -0400

    fully implement tar deduplication

--- scripts/testing/find_missing_tars.py
+++ scripts/testing/find_missing_tars.py
@@ -8,6 +8,7 @@ sys.path.append(str(ROOT))  # isort: skip
 # fmt: on
 
 
+import shutil
 import sys
 import tarfile
 import traceback
@@ -16,6 +17,7 @@ from pathlib import Path
 from tarfile import ReadError, TarFile
 from typing import List, Optional, Union
 
+import numpy as np
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
 
@@ -76,14 +78,82 @@ def print_bugged_ckpts(root: Path = CKPTS) -> List[Path]:
     return missings
 
 
+def dedup_tars(root: Path, delete: bool = False) -> None:
+    tars = list(root.rglob("*.tar.gz"))
+    parents, counts = np.unique([tar.parent for tar in tars], return_counts=True)
+    unq_parents = parents[counts > 1]
+    duplicates = []
+    dirs = []
+    corrupts = []
+    to_delete = []
+    for unq_parent in unq_parents:
+        files = list(unq_parent.rglob("*"))
+        file: Path
+        for file in files:
+            if "".join(file.suffixes) != ".tar.gz":
+                if file.is_dir():
+                    try:
+                        file.rmdir()
+                    except Exception:
+                        dirs.append(file)
+                else:
+                    file.unlink()
+            else:
+                duplicates.append(file)
+        targzs = filter(lambda f: "".join(f.suffixes) == ".tar.gz", files)
+        good_tars: List[Path] = []
+        for targz in targzs:
+            if is_bad_tar(targz):
+                corrupts.append(targz)
+            else:
+                good_tars.append(targz)
+        # sort so first item is most recent
+        goods = sorted(good_tars, key=lambda p: p.stat().st_mtime, reverse=True)
+        to_delete.extend(goods[1:])
+
+    # print("Directories with duplicate archives:")
+    # for dup in duplicates:
+    #     print(dup)
+    d: Path
+    print(f"Total archives found: {len(tars)}")
+    print(f"Leftover sub-directories: {len(dirs)}")
+    for d in dirs:
+        print(d)
+    print(f"Corrupt archives: {len(corrupts)}")
+    for c in corrupts:
+        print(c)
+    print(f"Duplicate archives to delete: {len(to_delete)}")
+    if len(to_delete) <= 10:
+        for d in to_delete:
+            print(d)
+    else:
+        for d in to_delete[:5]:
+            print(d)
+        print("...")
+        for d in to_delete[-5:]:
+            print(d)
+    if delete:
+        for d in to_delete:
+            d.unlink()
+
+
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument(
-        "--root",
-        type=Path,
-        required=True,
-        help="Directory from which to use Path.rglob('*.ckpt') on.",
-    )
-    args = parser.parse_args()
-    root = args.root
-    print_bugged_ckpts(root)
+    # parser = ArgumentParser()
+    # parser.add_argument(
+    #     "--root",
+    #     type=Path,
+    #     required=True,
+    #     help="Directory from which to use Path.rglob('*.ckpt') on.",
+    # )
+    # parser.add_argument(
+    #     "--tar-root",
+    #     type=Path,
+    #     required=True,
+    #     help="Directory to first dedup",
+    # )
+    # args = parser.parse_args()
+    # root = args.root
+    # print_bugged_ckpts(root)
+
+    PRELIM = ROOT / "debug_logs/prelim"
+    dedup_tars(PRELIM, delete=False)
