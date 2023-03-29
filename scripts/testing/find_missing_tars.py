04bac0605b4b0328d592d871bbf5d19cfe131e41
Derek Berger <dmberger.dev@gmail.com>
2023-03-29 12:00:07 -0400

    WIP

--- scripts/testing/find_missing_tars.py
+++ scripts/testing/find_missing_tars.py
@@ -10,6 +10,7 @@ sys.path.append(str(ROOT))  # isort: skip
 
 import sys
 import tarfile
+import traceback
 from argparse import ArgumentParser
 from pathlib import Path
 from tarfile import ReadError, TarFile
@@ -22,11 +23,42 @@ from src.constants import CKPTS
 from src.parallelize import joblib_map
 
 
+def is_bad_tar(tarpath: Path) -> bool:
+    with open(tarpath, "rb") as fp:
+        gz_archive: TarFile
+        try:
+            with tarfile.open(fileobj=fp, mode="r") as gz_archive:  # noqa
+                infos = gz_archive.getmembers()
+            if len(infos) < 5:
+                return True
+        except ReadError:
+            return True
+        except UnicodeDecodeError:
+            return True
+        except Exception:
+            traceback.print_exc()
+            return True
+    return False
+
+
+def find_bad_tars(root: Path) -> List[Path]:
+    tars = sorted(root.rglob("*.tar.gz"))
+    paths = process_map(is_bad_tar, tars, desc="Checking archives", chunksize=10)
+    paths = [p for p in paths if p is not None]
+    if len(paths) > 0:
+        print("Got bad archives:")
+        for path in paths:
+            print(path)
+    return paths
+
+
 def is_bugged_ckpt(ckpt: Path) -> Optional[Path]:
     try:
         with open(ckpt, "r") as fp:
             tarfile = Path(f"{fp.readline()[:-1]}.tar.gz").resolve()  # trailing newline
         if not tarfile.exists():
+            return tarfile
+        if is_bad_tar(tarfile):
             return tarfile
         return None
     except IOError:
@@ -41,28 +73,6 @@ def print_bugged_ckpts(root: Path = CKPTS) -> List[Path]:
     for missing in missings:
         print(missing)
     return missings
-
-
-def is_bad_tar(tarpath: Path) -> Union[Path, None]:
-    with open(tarpath, "rb") as fp:
-        gz_archive: TarFile
-        try:
-            with tarfile.open(fileobj=fp, mode="r") as gz_archive:  # noqa
-                ...
-        except ReadError:
-            return tarpath
-    return None
-
-
-def find_bad_tars(root: Path) -> List[Path]:
-    tars = sorted(root.rglob("*.tar.gz"))
-    paths = process_map(is_bad_tar, tars, desc="Checking archives", chunksize=10)
-    paths = [p for p in paths if p is not None]
-    if len(paths) > 0:
-        print("Got bad archives:")
-        for path in paths:
-            print(path)
-    return paths
 
 
 if __name__ == "__main__":
